982e6472121663124eda43d0c1cf8e2822ae8fa2
Pedro Lopes <pedroyremolo@gmail.com>
2019-06-20 12:19:42 -0300

    [ADD] Layout inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CardSliderComponent } from './card-slider/card-slider.component';
+import { CardComponent } from './card-slider/card/card.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardSliderComponent,
+    CardComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/card-slider/card-slider.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'dsw-card-slider',
+  templateUrl: './card-slider.component.html',
+  styleUrls: ['./card-slider.component.sass']
+})
+export class CardSliderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
